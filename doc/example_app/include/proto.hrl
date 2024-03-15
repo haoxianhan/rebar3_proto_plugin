123fea867450302f2b3d73a86c44ba4732836e2d
haoxian <haoxianhan@gmail.com>
2024-03-15 17:06:20 +0800

    update doc

--- doc/example_app/include/proto.hrl
+++ doc/example_app/include/proto.hrl
@@ -1,9 +1,9 @@
--define('SHOP_INFO', 5).
+-define('SHOP_INFO', 3).
 
--define('RESP_SHOP_INFO', 4).
+-define('RESP_SHOP_INFO', 2).
 
--define('RESP_HEARTBEAT', 2).
+-define('RESP_HEARTBEAT', 5).
 
--define('REQ_SHOP_INFO', 3).
+-define('REQ_SHOP_INFO', 1).
 
--define('REQ_HEARTBEAT', 1).
+-define('REQ_HEARTBEAT', 4).
--- doc/example_app/src/pb/proto_info.erl
+++ doc/example_app/src/pb/proto_info.erl
@@ -2,9 +2,9 @@
 
 -export([get_msg_handle/1]).
 
-get_msg_handle(1) -> handle_client;
+get_msg_handle(4) -> handle_client;
+get_msg_handle(1) -> handle_shop;
+get_msg_handle(5) -> handle_client;
+get_msg_handle(2) -> handle_shop;
 get_msg_handle(3) -> handle_shop;
-get_msg_handle(2) -> handle_client;
-get_msg_handle(4) -> handle_shop;
-get_msg_handle(5) -> handle_shop;
 get_msg_handle(_) -> undefined.
