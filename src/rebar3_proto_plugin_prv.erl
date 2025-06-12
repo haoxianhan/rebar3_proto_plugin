59ff21154b04b696b57cfdea1c82a1956285811e
haoxian <haoxianhan@gmail.com>
2025-06-12 11:15:05 +0800

    fix app_discovery namespace error

--- src/rebar3_proto_plugin_prv.erl
+++ src/rebar3_proto_plugin_prv.erl
@@ -3,7 +3,7 @@
 -export([init/1, do/1, format_error/1]).
 
 -define(PROVIDER, generate).
--define(DEPS, [app_discovery]).
+-define(DEPS, [{default, app_discovery}]).
 
 %% ===================================================================
 %% Public API
